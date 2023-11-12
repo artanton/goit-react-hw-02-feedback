c67975243e502b0abad52a11f22fbfd615f5cc6b
artanton <kabletrade@gmail.com>
2023-11-12 12:17:20 +0200

    HW-react-2-4

--- src/components/Statistic/Statistics.js
+++ src/components/Statistic/Statistics.js
@@ -1,21 +1,23 @@
+import { StatList } from './StatisticsStyled';
+
 export const Statistics = ({
   value: { good, neutral, bad },
   total,
   positiveFeadback,
 }) => {
   return (
-    <div>
+    <StatList>
       {total > 0 ? (
         <>
-          <p>Good:{good} </p>
-          <p>Neutral:{neutral} </p>
-          <p>Bad:{bad} </p>
-          <p>Total:{total} </p>
-          <p>Positive Feadback:{positiveFeadback}% </p>
+          <p>Good: {good} </p>
+          <p>Neutral: {neutral} </p>
+          <p>Bad: {bad} </p>
+          <p>Total: {total} </p>
+          <p>Positive Feadback: {positiveFeadback}% </p>
         </>
       ) : (
         <p>There is no feedback</p>
       )}
-    </div>
+    </StatList>
   );
 };
